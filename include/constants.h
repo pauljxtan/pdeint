6662bde8034e0ed54d901200709e80bcfe506ae9
Paul J.X. Tan <pjxtan@gmail.com>
2014-10-24 11:39:24 -0400

    Planck constant and electron mass

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+/* Various physical constants (in SI units). */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/* Reduced Planck constant [J s] */
+static const double hbar = 1.054571726e-34;
+
+/* Electron mass [kg] */
+static const double m_e = 9.10938215e-31;
+
+#endif /* CONSTANTS_H */
