4e6e7437a612b730b3b3e782a79369dd7416b954
Paul Tan <pjxtan@gmail.com>
2014-11-08 11:50:53 -0500

    Put real and imaginary in separate subplots; read framecount from argv

--- src/plot_stdin.py
+++ src/plot_stdin.py
@@ -4,19 +4,25 @@ import matplotlib.pyplot as plt
 import sys
 
 def main():
-    frameskip = 5
+    frameskip = int(sys.argv[1])
     plt.ion()
 
-    framecount = 0
-    N = 0
-    data = []
     fig = plt.figure()
-    sp = fig.add_subplot(111)
-    sp.set_ylim(-1, 1)
-    sp.set_ylim(-1, 1)
+    #sp = fig.add_subplot(111)
+    #sp.set_ylim(0, 1)
+    #sp.set_ylim(0, 1)
+    sp1 = fig.add_subplot(211)
+    sp1.set_ylim(-1, 1)
+    sp1.set_ylim(-1, 1)
+    sp2 = fig.add_subplot(212)
+    sp2.set_ylim(-1, 1)
+    sp2.set_ylim(-1, 1)
+    #line = None
     line1 = None
     line2 = None
     
+    framecount = 0
+    data = []
     while True:
         data = sys.stdin.readline()
         if data == '':
@@ -26,14 +32,15 @@ def main():
             continue
         data = data.split(" ")
         data = map(float, data)
-        sp.set_title(str(framecount))
+        sp1.set_title("%05d" % framecount)
+        #if line is None:
         if line1 is None:
             # If plotting absolute value:
             #line, = sp.plot(range(len(data)), data)
 
             # If plotting real and imaginary separately:
-            line1, = sp.plot(range(len(data) / 2), data[:len(data) / 2])
-            line2, = sp.plot(range(len(data) / 2), data[len(data) / 2:])
+            line1, = sp1.plot(range(len(data) / 2), data[:len(data) / 2], 'b-')
+            line2, = sp2.plot(range(len(data) / 2), data[len(data) / 2:], 'r-')
         else:
             # If plotting absolute value:
             #line.set_xdata(range(len(data)))
@@ -45,7 +52,8 @@ def main():
             line2.set_xdata(range(len(data) / 2))
             line2.set_ydata(data[len(data) / 2:])
 
-        #plt.savefig("./frames1/"+str(framecount)+".png")
+        #plt.savefig("/home/paultan/sims/piab/1/%04d.png" % framecount)
+        #print framecount
         plt.draw()
 
 if __name__ == "__main__":
